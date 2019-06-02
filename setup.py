85c256f6aa21c844d6b7d120605acfe407696800
Jeffrey Bush <jeff@coderforlife.com>
2019-06-02 02:35:34 -0400

    Update setup.py to be more complete for pypi.

--- setup.py
+++ setup.py
@@ -23,14 +23,26 @@ with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "README.md")
 
 setup(
     name='yarc',
-    version='0.9',
+    version='0.9.1',
     description='Yet Another Roomba Controller',
     license="GPLv3",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Jeffrey Bush',
     author_email='jeff@coderforlife.com',
     url="https://github.com/coderforlife/yarc",
     packages=['yarc'],
     install_requires=['pyserial'],
     python_requires='>=3.5',
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: System :: Hardware",
+    ],
 )
